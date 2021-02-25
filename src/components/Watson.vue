feb7e497d7d370a00e3b4dea2d9153ca6c5d43f7
TNTBlasterZapper <LordPenguin6@yahoo.com>
2021-02-25 09:15:56 -0500

    Created Component 3

--- src/components/Watson.vue
+++ src/components/Watson.vue
@@ -1,15 +1,18 @@
 <template>
     <div>
         <watson-comp-1 />
+        <watson-comp-3 text="Below Are Some Examples" />
     </div>
 </template>
 
 <script>
 import WatsonComp1 from './Watson/WatsonComp1.vue'
+import WatsonComp3 from './Watson/WatsonComp3.vue'
 export default {
     
     components: {
-        WatsonComp1
+        WatsonComp1,
+        WatsonComp3
         
     }
 
--- src/components/Watson/WatsonComp3.vue
+++ src/components/Watson/WatsonComp3.vue
@@ -1,12 +1,16 @@
 <template>
-    <div />
+    <div>{{text}}</div>
 </template>
 
 <script>
 export default {
-    
+    props: ["text"]
 }
 </script>
 
 <style scoped>
+    div {
+        width: 100%;
+        border-bottom-style: dashed;
+    }
 </style>

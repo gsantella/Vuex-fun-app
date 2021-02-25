5b389b2ddeca304cc133dbb70b599e966d8a871d
dlaff666 <70662517+dlaff666@users.noreply.github.com>
2021-02-25 11:13:52 -0500

    Added components 1-4

--- src/components/David/DavidComp1.vue
+++ src/components/David/DavidComp1.vue
@@ -1,13 +1,21 @@
 <template>
     <div>
+        <Button v-on:click="rollDie">Roll Die</Button>
+        <p>{{ result }}</p>
     </div>
 </template>
-
 <script>
 export default {
-    
+    data(){
+        return{
+            result:''
+        }
+    },
+    methods:{
+        rollDie(){
+            let randomNumber = Math.random()*6
+            this.result = Math.ceil(randomNumber)
+        }
+    }
 }
 </script>
-
-<style scoped>
-</style>
--- src/components/David/DavidComp3.vue
+++ src/components/David/DavidComp3.vue
@@ -1,12 +1,20 @@
 <template>
-    
+    <div>
+        <p>A coin was flipped.</p>
+        <p v-if="number%2 === 0">It was heads!</p>
+        <p v-else>It was tails!</p>
+    </div>
 </template>
-
 <script>
 export default {
-    
+    data(){
+        return{
+            number: 0
+        }
+    },
+    created(){
+        let randomNumber = Math.random()*10
+        this.number = Math.ceil(randomNumber)
+    }
 }
 </script>
-
-<style scoped>
-</style>

c49ce9b684401c8d32bbf9a424d351526b2733f6
GavinBlack <70662475+GavinBlack@users.noreply.github.com>
2021-02-25 09:20:36 -0500

    Added Components

---
+++ src/components/Gavin/GavinComp1.vue
@@ -0,0 +1,30 @@
+<template>
+    <div><hr>
+        <input id="input1" type="text">
+        <p>You entered {{text}}</p>
+        <button id="btn1" v-on:click="putInBox">Click</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+            text:''
+        }
+    },
+    methods: {
+        putInBox() {
+            this.text = document.getElementById('input1').value;
+        }
+    }
+    
+
+    
+}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Gavin/GavinComp4.vue
@@ -0,0 +1,25 @@
+<template>
+    <div><hr>
+        <p>ISS current latitude: {{latitude}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+            latitude:''
+        }
+    },
+
+    created() {
+        fetch('https://api.wheretheiss.at/v1/satellites/25544')
+            .then(response => response.json())
+            .then(data => this.latitude = data.latitude);
+    }
+}
+</script>
+
+
+<style scoped>
+</style>

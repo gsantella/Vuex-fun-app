ed6d7637090d56ce8ae332ba06eac9786f7fe5ef
TNTBlasterZapper <LordPenguin6@yahoo.com>
2021-02-25 09:48:21 -0500

    Created Component 4

--- src/components/Watson.vue
+++ src/components/Watson.vue
@@ -2,17 +2,21 @@
     <div>
         <watson-comp-1 />
         <watson-comp-3 text="Below Are Some Examples" />
+        <div class="box"><watson-comp-4 /></div>
+        <div class="box"><watson-comp-4 /></div>
     </div>
 </template>
 
 <script>
 import WatsonComp1 from './Watson/WatsonComp1.vue'
 import WatsonComp3 from './Watson/WatsonComp3.vue'
+import WatsonComp4 from './Watson/WatsonComp4.vue'
 export default {
     
     components: {
         WatsonComp1,
-        WatsonComp3
+        WatsonComp3,
+        WatsonComp4
         
     }
 
@@ -20,5 +24,10 @@ export default {
 </script>
 
 <style scoped>
-
+    .box
+    {
+        width: 49%;
+        border-style: solid;
+        display: inline-block;
+    }
 </style>

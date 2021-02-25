a32c02224410989b8c4477620d8245d873cd5901
TNTBlasterZapper <LordPenguin6@yahoo.com>
2021-02-25 09:55:25 -0500

    Created Component 5

--- src/components/Watson.vue
+++ src/components/Watson.vue
@@ -3,7 +3,7 @@
         <watson-comp-1 />
         <watson-comp-3 text="Below Are Some Examples" />
         <div class="box"><watson-comp-4 /></div>
-        <div class="box"><watson-comp-4 /></div>
+        <div class="box">Loading<watson-comp-5 /></div>
     </div>
 </template>
 
@@ -11,12 +11,14 @@
 import WatsonComp1 from './Watson/WatsonComp1.vue'
 import WatsonComp3 from './Watson/WatsonComp3.vue'
 import WatsonComp4 from './Watson/WatsonComp4.vue'
+import WatsonComp5 from './Watson/WatsonComp5.vue'
 export default {
     
     components: {
         WatsonComp1,
         WatsonComp3,
-        WatsonComp4
+        WatsonComp4,
+        WatsonComp5
         
     }
 
--- src/components/Watson/WatsonComp5.vue
+++ src/components/Watson/WatsonComp5.vue
@@ -1,12 +1,36 @@
 <template>
-    <div />
+    <div>{{text}}</div>
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return{
+            text: "."
+        }
+    },
+    created() {
+    this.interval = setInterval(() => this.loading(), 750);
+    },
+    methods:
+    {
+        loading()
+        {
+            if (this.text == "...")
+            {
+                this.text = "."
+            }
+            else
+            {
+                this.text = this.text + "."
+            }
+        }
+    }
 }
 </script>
 
 <style scoped>
+    div {
+        display: inline;
+    }
 </style>

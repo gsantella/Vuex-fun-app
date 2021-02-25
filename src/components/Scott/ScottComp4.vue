a1ca21173bea0cb7d459eb048c20ac6722af76ef
Scott <sagoss97@gmail.com>
2021-02-25 09:17:19 -0500

    Added 2 Components
    Added 2 more Vue Components and deleted a not needed file

--- src/components/Scott/ScottComp4.vue
+++ src/components/Scott/ScottComp4.vue
@@ -1,11 +1,35 @@
 <template>
-    
+  <div :style="{ backgroundColor: backgroundcolor }">
+    <button @click="start">New color</button>
+  </div>
 </template>
 
 <script>
 export default {
-    
-}
+  data() {
+    return {
+      backgroundcolor: "#000000",
+      color: [
+        "#0000ff",
+        "#00ffff",
+        "#00ff00",
+        "#ccff66",
+        "#ff6600",
+        "#ff3399",
+        "#ff00ff",
+        "#6666ff",
+        "#ffffff",
+        "#003300",
+        "#660066",
+      ],
+    };
+  },
+  methods: {
+    start() {
+      this.backgroundcolor = this.color[Math.floor(Math.random() * 10)];
+    },
+  },
+};
 </script>
 
 <style scoped>
--- src/components/Scott/ScottComp5.vue
+++ src/components/Scott/ScottComp5.vue
@@ -1,11 +1,19 @@
 <template>
-    
+  <div>
+    <label>This in put box submits with the release of the enter key</label>
+    <br />
+    <input @keyup.enter="greet()" />
+  </div>
 </template>
 
 <script>
 export default {
-    
-}
+  methods: {
+    greet: function (event) {
+      alert("You Submit using the enter button");
+    },
+  },
+};
 </script>
 
 <style scoped>

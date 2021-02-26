247ff5c4ff3d0519bf78a1abcc02c2cbeccc4a1b
Scott <sagoss97@gmail.com>
2021-02-25 20:55:24 -0500

    Added 8th and 9th component

--- src/components/Scott/ScottComp1.vue
+++ src/components/Scott/ScottComp1.vue
@@ -1,25 +1,12 @@
 <template>
-  <div>
-    <p>{{ name }}</p>
-  </div>
+      <div> 
+        <h1> Welcome to Scott's Components </h1>
+      </div>
 </template>
 
 <script>
 export default {
-  props: ["id"],
-
-  data() {
-    return {
-      name: "test",
-    };
-  },
-
-  created() {
-    fetch(`https://anapioficeandfire.com/api/houses/${this.id}`)
-      .then((response) => response.json())
-      .then((data) => (this.name = data.name));
-  },
-};
+}
 </script>
 
 <style scoped>

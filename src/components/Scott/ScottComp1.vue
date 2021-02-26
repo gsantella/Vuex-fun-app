89716d3cce277aaa0bf912a988b7fac79eb9dd2a
Scott <sagoss97@gmail.com>
2021-02-25 20:57:17 -0500

    Code Clean Up

--- src/components/Scott/ScottComp1.vue
+++ src/components/Scott/ScottComp1.vue
@@ -1,13 +1,19 @@
 <template>
-      <div> 
-        <h1> Welcome to Scott's Components </h1>
-      </div>
+  <div>
+    <h1>Welcome to Scott's Components</h1>
+    <p>You will see the components that Scott made below</p>
+    <img
+      src="https://i.insider.com/5e32f2a324306a19834af322?width=1000&format=jpeg&auto=webp"
+    />
+  </div>
 </template>
 
 <script>
-export default {
-}
+export default {};
 </script>
 
 <style scoped>
+img {
+  width: 60%;
+}
 </style>

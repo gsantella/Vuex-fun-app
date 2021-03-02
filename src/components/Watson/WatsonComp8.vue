e3cbee6332a36316a4077e8d57510375e9c316ff
TNTBlasterZapper <LordPenguin6@yahoo.com>
2021-03-02 09:46:18 -0500

    Created Component 8

--- src/components/Watson/WatsonComp8.vue
+++ src/components/Watson/WatsonComp8.vue
@@ -1,10 +1,22 @@
 <template>
-    <div />
+    <div >Your Pokemon is {{name}}</div>
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return{
+            name:'',
+            randomNumber: 1
+        }
+    },
+
+    created() {
+        this.randomNumber = Math.floor((Math.random() * 893) + 1);
+        fetch('https://pokeapi.co/api/v2/pokemon-form/' + this.randomNumber)
+            .then(response => response.json())
+            .then(data => this.name = data.name);
+    }
 }
 </script>
 

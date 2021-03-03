dbdf9e63acbcae5c5b8ba57fb330d373e0d5fbae
dlaff666 <70662517+dlaff666@users.noreply.github.com>
2021-03-02 19:56:05 -0500

    Added Components 7-9
    Also made changes to component 5 and main tag.

--- src/components/David/DavidComp7.vue
+++ src/components/David/DavidComp7.vue
@@ -1,12 +1,27 @@
 <template>
-    
+    <div>
+        <h3>Robot Generator</h3>
+        <img v-bind:src="robotPicLink" alt="The pic for the text you entered.">
+        <form v-on:submit="getLink">
+            <label for="input">Enter any text here:</label>
+            <input type="text" v-model="input">
+            <button type="submit">Get a Robot</button>
+        </form>
+    </div>
 </template>
-
 <script>
 export default {
-    
+    data(){
+        return{
+            input:"",
+            robotPicLink:"https://robohash.org/sample"
+        }
+    },
+    methods:{
+        getLink(e){
+            e.preventDefault()
+            this.robotPicLink = "https://robohash.org/" + this.input
+        }
+    }
 }
 </script>
-
-<style scoped>
-</style>

7d5e3117368c631afe79a9d1afa23204b943db14
S-goss <70651880+S-goss@users.noreply.github.com>
2021-03-08 13:02:23 -0500

    Updated Scott.vue Final

--- src/components/Scott.vue
+++ src/components/Scott.vue
@@ -15,15 +15,15 @@
 </template>
 
 <script>
-import ScottComp1 from "./ScottComp1";
-import ScottComp2 from "./ScottComp2";
-import ScottComp3 from "./ScottComp3";
-import ScottComp4 from "./ScottComp4";
-import ScottComp5 from "./ScottComp5";
-import ScottComp6 from "./ScottComp6";
-import ScottComp7 from "./ScottComp7";
-import ScottComp8 from "./ScottComp8";
-import ScottComp9 from "./ScottComp9";
+import ScottComp1 from "./Scott/ScottComp1";
+import ScottComp2 from "./Scott/ScottComp2";
+import ScottComp3 from "./Scott/ScottComp3";
+import ScottComp4 from "./Scott/ScottComp4";
+import ScottComp5 from "./Scott/ScottComp5";
+import ScottComp6 from "./Scott/ScottComp6";
+import ScottComp7 from "./Scott/ScottComp7";
+import ScottComp8 from "./Scott/ScottComp8";
+import ScottComp9 from "./Scott/ScottComp9";
 
 export default {
   name: "App",

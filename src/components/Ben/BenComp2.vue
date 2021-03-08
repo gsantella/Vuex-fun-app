2e44e70bb0cd48d999852f88bf6a4e4aab2474e0
bduclo22 <70662467+bduclo22@users.noreply.github.com>
2021-03-07 19:46:18 -0500

    Added components

--- src/components/Ben/BenComp2.vue
+++ src/components/Ben/BenComp2.vue
@@ -1,12 +1,35 @@
 <template>
-    
+  <div class="container">
+    <h1>Background Image</h1>
+    <p>Enter Image URL:</p>
+    <div :style="{ backgroundImage: `url(${backgroundURL})` }">
+      <input type="text" id="imageURL" v-model="imageURL" name="imageURL">
+      <button @click="changeBackground">Submit</button>
+    </div>
+  </div>
 </template>
 
 <script>
+
 export default {
-    
-}
+  data() {
+    return {
+      backgroundURL:""
+    };
+  },
+  methods: {
+    changeBackground() {
+
+      var URL = document.querySelector("input[name=imageURL]").value
+      this.backgroundURL = URL
+
+    },
+  },
+};
+
 </script>
 
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
 </style>
--- src/components/Ben/BenComp7.vue
+++ src/components/Ben/BenComp7.vue
@@ -1,12 +1,29 @@
 <template>
-    
+    <div>
+        <h1>Date and Time</h1>
+        <h1>{{ time }}</h1>
+    </div>
 </template>
-
 <script>
+
 export default {
-    
+    data(){
+        return{
+          time: ""
+        }
+    },
+    created() {
+                setInterval(this.getNow, 1000);
+    },
+    methods:{
+
+    getNow: function() {
+                const today = new Date();
+                const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+                const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+                const dateTime = date +' '+ time;
+                this.time = dateTime;
+            }
+    }
 }
 </script>
-
-<style scoped>
-</style>

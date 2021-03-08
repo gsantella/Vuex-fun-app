207fa093c68833fe9a7c5568589fda14f8f6f894
S-goss <70651880+S-goss@users.noreply.github.com>
2021-03-08 12:46:36 -0500

    Updated Scott.vue
    Added content that shouldn't break

--- src/components/Scott.vue
+++ src/components/Scott.vue
@@ -1,18 +1,45 @@
 <template>
-    <div>
-    </div>
+  <div>
+    <scott-comp-1 />
+    <scott-comp-2 />
+    <scott-comp-3 />
+    <scott-comp-4 />
+    <scott-comp-5 />
+    <scott-comp-6 />
+    <scott-comp-7 />
+    <scott-comp-8 />
+    <scott-comp-9 id="3" />
+    <scott-comp-9 id="5" />
+    <scott-comp-9 id="7" />
+  </div>
 </template>
 
 <script>
-export default {
-    
-    components: {
-        
-    }
+import ScottComp1 from "./components/ScottComp1";
+import ScottComp2 from "./components/ScottComp2";
+import ScottComp3 from "./components/ScottComp3";
+import ScottComp4 from "./components/ScottComp4";
+import ScottComp5 from "./components/ScottComp5";
+import ScottComp6 from "./components/ScottComp6";
+import ScottComp7 from "./components/ScottComp7";
+import ScottComp8 from "./components/ScottComp8";
+import ScottComp9 from "./components/ScottComp9";
 
-}
+export default {
+  name: "App",
+  components: {
+    ScottComp1,
+    ScottComp2,
+    ScottComp3,
+    ScottComp4,
+    ScottComp5,
+    ScottComp6,
+    ScottComp7,
+    ScottComp8,
+    ScottComp9,
+  },
+};
 </script>
 
-<style scoped>
-
+<style>
 </style>
